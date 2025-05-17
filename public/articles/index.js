d22c97874004b5b66b6c7e67690c523cb21da965
Daniel Gilbert <ich@danielgilbert.de>
2025-05-17 13:11:52 +0200

    Reorders more files

--- public/articles/index.js
+++ public/articles/index.js
@@ -1,4 +1,4 @@
-import { registerWebsiteHeader } from "./components/website-header/website-header.js";
+import { registerWebsiteHeader } from "../components/website-header/website-header.js";
 import { registerWebsiteFooter } from "./../components/website-footer/website-footer.js";
 
 const app = async () => {
