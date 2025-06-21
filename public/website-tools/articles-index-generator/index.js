a62710e299e410e5900cd7e9d7462a8111935dc6
Daniel Gilbert <ich@danielgilbert.de>
2025-06-21 08:34:37 +0200

    Pre-work on tags

---
+++ public/website-tools/articles-index-generator/index.js
@@ -0,0 +1,12 @@
+import { registerWebsiteHeader } from "../../components/website-header/website-header.js";
+import { registerWebsiteFooter } from "../../components/website-footer/website-footer.js";
+import { registerArticlesIndexGenerator } from "./components/articles-index-generator/articles-index-generator.js"
+
+
+const app = async () => {
+    registerWebsiteHeader();
+    registerWebsiteFooter();
+    registerArticlesIndexGenerator();
+}
+
+document.addEventListener('DOMContentLoaded', app);
